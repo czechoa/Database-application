d46b4fc30877256d63830c519b783486689216e9
czechoa <andrzej.czechu.ja@gmail.com>
2021-10-14 14:32:55 +0200

    finnish lesson 4 ( adding tests and get_cleaning_by_id)

--- backend/app/api/routes/cleanings.py
+++ backend/app/api/routes/cleanings.py
@@ -1,6 +1,17 @@
 from typing import List
-from fastapi import APIRouter
+
+
+from fastapi import APIRouter, Body, Depends, HTTPException
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
+
+from app.models.cleaning import CleaningCreate, CleaningPublic
+from app.db.repositories.cleanings import CleaningsRepository
+from app.api.dependencies.database import get_repository
+
+
 router = APIRouter()
+
+
 @router.get("/")
 async def get_all_cleanings() -> List[dict]:
     cleanings = [
@@ -8,3 +19,23 @@ async def get_all_cleanings() -> List[dict]:
         {"id": 2, "name": "Someone else's house", "cleaning_type": "spot_clean", "price_per_hour": 19.99}
     ]
     return cleanings
+
+@router.get("/{id}/", response_model=CleaningPublic, name="cleanings:get-cleaning-by-id")
+async def get_cleaning_by_id(
+  id: int, cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository))
+) -> CleaningPublic:
+    cleaning = await cleanings_repo.get_cleaning_by_id(id=id)
+    if not cleaning:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No cleaning found with that id.")
+    return cleaning
+
+
+@router.post("/", response_model=CleaningPublic, name="cleanings:create-cleaning", status_code=HTTP_201_CREATED)
+async def create_new_cleaning(
+        new_cleaning: CleaningCreate = Body(..., embed=True),
+        cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository)),
+) -> CleaningPublic:
+    created_cleaning = await cleanings_repo.create_cleaning(new_cleaning=new_cleaning)
+    return created_cleaning
+
+
