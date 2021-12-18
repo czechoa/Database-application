3010d0b50f17ac3c6bc8463eb6e07731d660cafe
czechoa <andrzej.czechu.ja@gmail.com>
2021-12-18 17:46:43 +0100

    change tutle project

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from databases import DatabaseURL
 from starlette.config import Config
 from starlette.datastructures import Secret
 config = Config(".env")
-PROJECT_NAME = "phresh"
+PROJECT_NAME = "Online_courses"
 VERSION = "1.0.0"
 API_PREFIX = "/api"
 
